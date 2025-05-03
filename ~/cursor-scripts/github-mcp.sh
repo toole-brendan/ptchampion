c93bcc338e165622e89342fb8cf4cca9130c07be
Brendan Toole <brendantoole@brendans-mbp.mynetworksettings.com>
2025-05-02 22:30:55 -0400

    Auto-commit: Fri May  2 22:30:55 EDT 2025

---
+++ ~/cursor-scripts/github-mcp.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+npx -y @smithery/cli@latest run @smithery-ai/github --key 6a607d4e-38d0-4ce1-801d-073a3a51cd97 --config '{"githubPersonalAccessToken":"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"}' 
